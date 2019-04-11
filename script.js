7781b71c34cad25da94d89faed7d36c184d681fb
regbadminton <49506031+regbadminton@users.noreply.github.com>
2019-04-11 14:20:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ var courseDate=new Date(midnightTs);
 var main='For '+days[courseDate.getDay()]+' '+months[courseDate.getMonth()]+' '+courseDate.getDate()+' '+courseDate.getFullYear()+':'+nl;
 
 if (courses.length==2) main+='Slot A: '+courses[0].Barcode+nl+'Slot B: '+courses[1].Barcode;
-else if(courses.length==1) main+='Barcode '+courses[0].Barcode+nl+'Barcodes for Sunday will be shown after 6pm on Saturday';
+else if (courses.length==1) main+='Barcode '+courses[0].Barcode+nl+'Barcodes for Sunday will be shown after 6pm on Saturday';
 else if (courses.length==0) main+='No barcodes found.';
 
 document.getElementsByTagName("main")[0].innerHTML=main;
