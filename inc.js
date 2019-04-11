c7d5cbf7e528ba4a4093c3dcded38a55ece6316b
regbadminton <49506031+regbadminton@users.noreply.github.com>
2019-04-11 13:29:39 -0700

    Update inc.js

--- inc.js
+++ inc.js
@@ -1,4 +1,3 @@
-
 var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var nl='<br>';
