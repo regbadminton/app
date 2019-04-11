e405bf7169d2e7b60f23f2f78e26c0e621a62a52
regbadminton <49506031+regbadminton@users.noreply.github.com>
2019-04-11 13:27:11 -0700

    Add files via upload

---
+++ inc.js
@@ -0,0 +1,15 @@
+
+var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var nl='<br>';
+var ONE_DAY=86400000;
+var EIGHTEEN_HOURS=64800000;
+
+function getCourses(timestamp) {
+    var total = arr.length;
+    var courses=[];
+    timestamp=timestamp/1000;
+    for (var i = 0; i < total; i++)
+        if (arr[i].Timestamp==timestamp) courses.push(arr[i]);
+    return courses;
+}
