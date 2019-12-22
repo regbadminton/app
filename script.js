6cf77a4c25e3894d3eeb2162c077426c60b7dd68
Michael Levitski <levitski.michael@gmail.com>
2019-12-21 20:16:48 -0800

    change to await

--- script.js
+++ script.js
@@ -9,38 +9,36 @@ let courses=[];
 tomorrow.setDate(tomorrow.getDate()+1);
 let strTomorrow=tomorrow.toLocaleString('ja-JP',DATE_FORMAT).split('/').reduce((holder,value)=>holder+=value);
 
-fetch('https://regbadminton.com/api/?d='+strTomorrow).catch(error=>console.error(error))
-    .then(response=>response.json()).then(json=>{
-        courses=json.map(obj=>new Course(obj));
-        switch (courses.length) {
-            case 2:
-                const button2=button1.cloneNode();
-                button2.className='btn btn-light px-5 ml-5';  
-                courses[1].displayButton(button2,2);
+(async function(){
+    courses=(await(await fetch('https://regbadminton.com/api/?d='+strTomorrow)).json()).map(obj=>new Course(obj));
+    switch (courses.length) {
+        case 2:
+            const button2=button1.cloneNode();
+            button2.className='btn btn-light px-5 ml-5';  
+            courses[1].displayButton(button2,2);
 
-                button1.insertAdjacentElement('afterend',button2);
-                button1.insertAdjacentElement('afterend',document.createElement('br'));
-                button1.className='btn btn-light my-3 px-5 ml-5';  
-            case 1:
-                courses[0].displayButton(button1,1);
-                button1.classList.remove('d-none');
-            break;
-    
-            default:
-                let div=document.createElement('div');
-                div.className="mt-3 ml-5";
-                div.textContent='No sessions found.';
-                button1.insertAdjacentElement('afterend',div);
-            break;
-        }
-        button1.insertAdjacentText('beforebegin',"For "+tomorrow.toLocaleString(LOCALE,HEADER_FORMAT)+':');
-        button1.insertAdjacentElement("beforebegin",document.createElement('br'));
+            button1.insertAdjacentElement('afterend',button2);
+            button1.insertAdjacentElement('afterend',document.createElement('br'));
+            button1.className='btn btn-light my-3 px-5 ml-5';  
+        case 1:
+            courses[0].displayButton(button1,1);
+            button1.classList.remove('d-none');
+        break;
 
-        document.querySelector("footer span").innerHTML=new Date().toLocaleString(LOCALE,UPDATED_FORMAT);
-        document.querySelector('footer a').href='https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4BookingPages/Classes?calendarId=ec6defcd-4317-4bf3-a72e-a9c6b4e5c897&widgetId=15f6af07-39c5-473e-b053-96653f77a406&embed=False';
-        document.querySelector('footer').classList.remove('d-none');
+        default:
+            let div=document.createElement('div');
+            div.className="mt-3 ml-5";
+            div.textContent='No sessions found.';
+            button1.insertAdjacentElement('afterend',div);
+        break;
     }
-);    
+    button1.insertAdjacentText('beforebegin',"For "+tomorrow.toLocaleString(LOCALE,HEADER_FORMAT)+':');
+    button1.insertAdjacentElement("beforebegin",document.createElement('br'));
+
+    document.querySelector("footer span").innerHTML=new Date().toLocaleString(LOCALE,UPDATED_FORMAT);
+    document.querySelector('footer a').href='https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4BookingPages/Classes?calendarId=ec6defcd-4317-4bf3-a72e-a9c6b4e5c897&widgetId=15f6af07-39c5-473e-b053-96653f77a406&embed=False';
+    document.querySelector('footer').classList.remove('d-none');
+})();
 
 class Course{
     constructor(obj){
