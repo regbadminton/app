cf203b420b143fd887d4035052b8b1efd95ad31e
Michael Levitski <levitski.michael@gmail.com>
2020-02-02 14:15:25 -0800

    alert debug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator)
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/reg/sw.js')
   .then(reg => alert("Service Worker Registered",reg))
   .catch(err => alert(err));
 
