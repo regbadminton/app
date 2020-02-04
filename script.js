7222415d7fcdeada2a0ef3c257974a28cbdc1a02
Michael Levitski <levitski.michael@gmail.com>
2020-02-03 19:48:45 -0800

    fix sw ref

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator)
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/reg/sw.js')
   .then(reg => console.log("Service Worker Registered",reg))
   .catch(err => console.warn(err));
 
