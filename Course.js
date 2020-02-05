758a27a789bdef8dd746778ae041b68cd0ee04a5
Michael Levitski <levitski.michael@gmail.com>
2020-02-05 14:29:25 -0800

    move initialization of buttons into Course class and change handling of d-none class

--- Course.js
+++ Course.js
@@ -1,15 +1,15 @@
 class Course{
-
     constructor(obj){
         this.date = obj.date.replace(/-/g, '');
         this.classID = obj.classID;
         this.locationID = obj.locationID;
         this.session = obj.session;
+        this.button = document.querySelectorAll('main a.btn')[this.session - 1];
     }
   
     createButton(){
-      buttons[this.session - 1].classList.remove('d-none');
-      buttons[this.session - 1].href = `https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4EventParticipants`+
+      this.button.classList.remove('d-none');
+      this.button.href = `https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4EventParticipants`+
                       `?eventId=${this.classID}`+
                       `&occurrenceDate=${this.date}`+
                       `&widgetId=15f6af07-39c5-473e-b053-96653f77a406`+
@@ -18,9 +18,9 @@ class Course{
     }
   
     static async fetchCourses(){
-      const visibleElements = document.querySelectorAll('header, footer, .spinner-border');
-      visibleElements.forEach(node => node.classList.toggle('d-none'));
-      document.querySelectorAll('main #row>*').forEach(element => element.classList.add('d-none'));
+      const spinner = document.querySelector('.spinner-border');
+      spinner.classList.remove('d-none');
+      document.querySelectorAll('main #row > *, header, footer').forEach(element => element.classList.add('d-none'));
       
       let dates = { today: new Date(), tomorrow: new Date() };
       dates.tomorrow.setDate(dates.today.getDate() + 1);
@@ -33,7 +33,7 @@ class Course{
       switch (courses.length) {
         case 2:
           courses[1].createButton();
-          buttons[0].className = 'ml-sm-3 mr-sm-2 mx-lg-0 btn btn-light d-none mb-3 col-sm-5 col-lg-12';
+          courses[0].button.className = 'ml-sm-3 mr-sm-2 mx-lg-0 btn btn-light mb-3 col-sm-5 col-lg-12';
         case 1:
           courses[0].createButton();
         break;
@@ -42,6 +42,7 @@ class Course{
           document.querySelector('#none').classList.remove('d-none');
         break;
       }
-      visibleElements.forEach(node => node.classList.toggle('d-none'));
+      spinner.classList.add('d-none');
+      document.querySelectorAll('header, footer').forEach(element => element.classList.remove('d-none'));
     }
   }
--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const DATE_FOMRAT = { weekday:'long',year:'numeric',month:'long',day:'numeric' }
 const UPDATED_FORMAT = { weekday:'short',year:'numeric',month:'short',day:'numeric',
                          hour:'numeric',minute:'numeric',hour12:true };
 
-const buttons = document.querySelectorAll('main a.btn');
 
 const searchDomain = 'https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4BookingPages/Classes';
 document.querySelector('footer #login').href += 'https://cityofsurrey.perfectmind.com/23615/Menu/Contact';
@@ -11,7 +10,7 @@ document.querySelector('footer #search').href = searchDomain +
                                                 '&widgetId=15f6af07-39c5-473e-b053-96653f77a406'+
                                                 '&embed=False';
 
-addEventListener('load',Course.fetchCourses);
+addEventListener('load', Course.fetchCourses);
 document.querySelectorAll('[name=dateSelect]').forEach(radio => radio.addEventListener('change', Course.fetchCourses));
 
 if ('serviceWorker' in navigator)
