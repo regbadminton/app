7b29ba6971cfdee5c8654212e8fd3b6be37702f8
Michael Levitski <levitski.michael@gmail.com>
2020-02-05 13:13:43 -0800

    fix bugs and add course to seperate file

--- script.js
+++ script.js
@@ -11,61 +11,8 @@ document.querySelector('footer #search').href = searchDomain +
                                                 '&widgetId=15f6af07-39c5-473e-b053-96653f77a406'+
                                                 '&embed=False';
 
-const toggleVisibility =()=> {
-  [document.querySelector('.spinner-border'),
-   document.querySelector('header'),
-   document.querySelector('main'),
-   document.querySelector('footer')
-  ].forEach(node => node.classList.toggle('d-none'));
-}
-
-const fetchCourses =async()=> {
-  toggleVisibility();
-  let dates = { today: new Date(), tomorrow: new Date() };
-  dates.tomorrow.setDate(dates.today.getDate() + 1);
-  let date = document.querySelector("input[name=dateSelect]:checked").value;
-  
-  document.querySelector('#date span').innerHTML = dates[date].toLocaleString('en-us', DATE_FOMRAT);
-  document.querySelector('#updated').innerHTML = dates.today.toLocaleString('en-us', UPDATED_FORMAT);
-
-  let courses = (await(await fetch('https://regbadminton.com/api/?d='+ date)).json()).map(obj => new Course(obj));
-  switch (courses.length) {
-    case 2:
-      courses[1].createButton();
-      buttons[0].className = 'ml-sm-3 mr-sm-2 mx-lg-0 btn btn-light d-none mb-3 col-sm-5 col-lg-12';
-    case 1:
-      courses[0].createButton();
-    break;
-  
-    default:
-      document.querySelector('#none').classList.remove('d-none');
-    break;
-  }
-  toggleVisibility();
-}
-
-addEventListener('load',fetchCourses);
-document.querySelectorAll('[name=dateSelect]').forEach(radio => radio.addEventListener('change', fetchCourses));
-
-class Course{
-
-  constructor(obj){
-      this.date = obj.date.replace(/-/g, '');
-      this.classID = obj.classID;
-      this.locationID = obj.locationID;
-      this.session = obj.session;
-  }
-
-  createButton(){
-    buttons[this.session - 1].classList.remove('d-none');
-    buttons[this.session - 1].href = `https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4EventParticipants`+
-                    `?eventId=${this.classID}`+
-                    `&occurrenceDate=${this.date}`+
-                    `&widgetId=15f6af07-39c5-473e-b053-96653f77a406`+
-                    `&locationId=${this.locationID}`+
-                    `&waitListMode=False`;
-  }
-}
+addEventListener('load',Course.fetchCourses);
+document.querySelectorAll('[name=dateSelect]').forEach(radio => radio.addEventListener('change', Course.fetchCourses));
 
 if ('serviceWorker' in navigator)
   navigator.serviceWorker.register('/app/sw.js')
--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ self.addEventListener("install", installEvent => {
       cache.addAll([
         "/",
         "/index.html",
+        "/Course.js",
         "/script.js",
         "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
         "/styles.css"
