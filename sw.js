aef863bf5e19d54669a38b81da81830d450cf2a1
Michael Levitski <levitski.michael@gmail.com>
2020-02-11 21:12:30 -0800

    add url to service worker

--- sw.js
+++ sw.js
@@ -1,40 +1,18 @@
-// const cacheName = 'regBadminton';
-// self.addEventListener("install", installEvent => {
-//   installEvent.waitUntil(
-//     caches.open(cacheName).then(cache => {
-//       cache.addAll([
-//         "/",
-//         "/index.html",
-//         "/Course.js",
-//         "/script.js",
-//         "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
-//         "/styles.css"
-//       ]);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", fetchEvent => {
-//   fetchEvent.respondWith(
-//     caches.match(fetchEvent.request).then(res => {
-//       return res || fetch(fetchEvent.request);
-//     })
-//   );
-// });
-
 self.addEventListener('push', event => {
   console.log('[Service Worker] push recieved');
-  const notification = JSON.parse(event.data.text());
-  console.log('Notification: ',notification);
-  const title = notification.title;
-  console.log('Title: ',notification.title);
-  console.log('Options: ',notification.options);
-  const options = notification.options;
+  const payload = JSON.parse(event.data.text());
+  console.log('Payload: ',payload);
 
+  self.pushUrl = payload.url;
+
+  const title = payload.title;
+  const options = payload.options;
   event.waitUntil(self.registration.showNotification(title, options))
 })
 
 self.addEventListener('notificationclick', event => {
   console.log('[Service Worker] notification clicked');
   event.notification.close();
+
+  event.waitUntil( clients.openWindow(self.pushUrl) );
 })
