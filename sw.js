eb3da110d8f8b61b94d76de345abd92dd571b024
Michael Levitski <levitski.michael@gmail.com>
2020-02-11 19:18:49 -0800

    add logs

--- sw.js
+++ sw.js
@@ -23,9 +23,12 @@
 // });
 
 self.addEventListener('push', event => {
-  console.log('[Service Worker] push recieved', event.data.text);
+  console.log('[Service Worker] push recieved');
   const notification = JSON.parse(event.data.text());
+  console.log('Notification: ',notification);
   const title = notification.title;
+  console.log('Title: ',notification.title);
+  console.log('Options: ',notification.options);
   const options = JSON.parse(notification.options);
 
   event.waitUntil(self.registration.showNotification(title, options))
